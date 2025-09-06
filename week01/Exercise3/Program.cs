58b178248ffebfa0c08de17b69b2a1ae14050344
Date-Masamune <sirbruce13@yahoo.com>
2025-09-06 01:03:29 -0600

    finished activity

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,36 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string keepPlaying = "yes";
+
+        while (keepPlaying.ToLower() == "yes")
+        {
+            Random rng = new Random();
+            int magicNumber = rng.Next(1, 101); // 1..100 inclusive
+            int guess = -1;
+            int guessCount = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                if (!int.TryParse(Console.ReadLine(), out guess))
+                {
+                    Console.WriteLine("Please enter a whole number.");
+                    continue;
+                }
+
+                guessCount++;
+
+                if (guess < magicNumber) Console.WriteLine("Higher");
+                else if (guess > magicNumber) Console.WriteLine("Lower");
+                else Console.WriteLine("You guessed it!");
+            }
+
+            Console.WriteLine($"It took you {guessCount} guesses.");
+            Console.Write("Do you want to continue? ");
+            keepPlaying = Console.ReadLine();
+        }
     }
 }
