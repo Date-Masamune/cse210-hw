72940dadd4120cedf6c71db1e2f63da610ee60ba
Date-Masamune <sirbruce13@yahoo.com>
2025-10-06 14:34:44 -0600

    done

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Test individual shapes
+        Square square = new Square("Red", 5);
+        Console.WriteLine($"Square Color: {square.GetColor()}");
+        Console.WriteLine($"Square Area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Console.WriteLine($"Rectangle Color: {rectangle.GetColor()}");
+        Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Green", 3);
+        Console.WriteLine($"Circle Color: {circle.GetColor()}");
+        Console.WriteLine($"Circle Area: {circle.GetArea()}");
+
+        Console.WriteLine("\n--- Polymorphism Example ---");
+
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Yellow", 2.5),
+            new Rectangle("Purple", 3, 7),
+            new Circle("Orange", 4)
+        };
+
+        // Iterate using polymorphism
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetColor()} shape area: {shape.GetArea():F2}");
+        }
     }
 }
---
+++ week06/Shapes/square.cs
@@ -0,0 +1,16 @@
+public class Square : Shape
+{
+    private double _side;
+
+    // Constructor calls base constructor
+    public Square(string color, double side) : base(color)
+    {
+        _side = side;
+    }
+
+    // Override the base class method
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}
